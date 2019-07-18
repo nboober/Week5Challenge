9517b3bd3dd5b9665ab4a8d01c9f454cc8cc7bee
nboober <nboober@terpmail.umd.edu>
2019-07-18 09:59:27 -0400

    Added a form for new messages and navigation to the page in my home controller

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,6 +16,12 @@ public class HomeController {
     public String showMessages(Model model){
         model.addAttribute("messages", messageRepository.findAll());
         return "home";
+    }
+
+    @GetMapping("/add")
+    public String newMessage(Model model){
+        model.addAttribute("message", new Bullhorn());
+        return "messageForm";
     }
 
 }
